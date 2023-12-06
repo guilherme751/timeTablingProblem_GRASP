f2867b530b92422052eaf453de283c0f8116d231
guilherme751 <guisgb10@gmail.com>
2023-12-06 17:33:52 -0300

    instances 1 to 14

--- runInst.py
+++ runInst.py
@@ -1,21 +1,21 @@
 from main import timeTablingInstance
 
 instances= [
-     "toy.ctt",    
+    # "toy.ctt",    
      "comp01.ctt",
-    # "comp02.ctt",
-    # "comp03.ctt",
-    # "comp04.ctt",
-    # "comp05.ctt",
-    # "comp06.ctt",
-    # "comp07.ctt",
-    # "comp08.ctt",
-    # "comp09.ctt",
-    # "comp10.ctt",
-    # "comp11.ctt",
-    # "comp12.ctt",
-    # "comp13.ctt",
-    # "comp14.ctt",
+     "comp02.ctt",
+     "comp03.ctt",
+     "comp04.ctt",
+     "comp05.ctt",
+     "comp06.ctt",
+     "comp07.ctt",
+     "comp08.ctt",
+     "comp09.ctt",
+     "comp10.ctt",
+     "comp11.ctt",
+     "comp12.ctt",
+     "comp13.ctt",
+     "comp14.ctt",
     # "comp15.ctt",
     # "comp16.ctt",
     # "comp17.ctt",
@@ -27,11 +27,23 @@ instances= [
 
 print("Running instances:\n")
 for instance in instances:
-    f = open("results/" + instance, "w")
-    print("Instance:", instance + "...")
-    for _ in range(5):
-        path = "data/" + instance
-        cost = timeTablingInstance(300, 0.1, path, 228)
-        f.write(str(cost) + " ")
+    f = open("results/" + instance, "r")
+    #print("Instance:", instance + "...")
+    #for _ in range(5):
+        #path = "data/" + instance
+        #cost = timeTablingInstance(300, 0.1, path, 228)
+        #f.write(str(cost) + " ")
+    x = f.readline().split(" ")
+    media = 0
+    for item in x:
+        if item == '':            
+            continue        
+        media += int(item)
+    media = media/5
+    print(media)
+    
     f.close()
+    
+    
+    
 
