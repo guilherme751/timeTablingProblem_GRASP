a626fe901229c5c17629ccfca1776162347aa0d7
guilherme751 <guisgb10@gmail.com>
2023-12-08 11:51:20 -0300

    A

--- runInst.py
+++ runInst.py
@@ -16,34 +16,31 @@ instances= [
     # "comp12.ctt",
     # "comp13.ctt",
     # "comp14.ctt",
-    # "comp15.ctt",
-    # "comp16.ctt",
-    # "comp17.ctt",
-    # "comp18.ctt",
-    # "comp19.ctt",
-    # "comp20.ctt",
+    "comp15.ctt",
+    "comp16.ctt",
+    "comp17.ctt",
+    "comp18.ctt",
+    "comp19.ctt",
+    "comp20.ctt",
      "comp21.ctt"
 ]
 
 print("Running instances:\n")
 for instance in instances:
-    f = open("results/" + instance, "w")
-    print("Instance:", instance + "...")
-    for _ in range(5):
-        path = "data/" + instance
-        cost = timeTablingInstance(300, 0.1, path, 228)
-        f.write(str(cost) + " ")
-    #x = f.readline().split(" ")
-    #media = 0
-    #for item in x:
-    #    if item == '':            
-    #        continue        
-    #    media += int(item)
-    #media = media/5
-    #print(media)
+    f = open("results/" + instance, "r")
+    #print("Instance:", instance + "...")
+    # for _ in range(5):
+    #     path = "data/" + instance
+    #     cost = timeTablingInstance(300, 0.1, path, 228)
+    #     f.write(str(cost) + " ")
+    x = f.readline().split(" ")
+    media = 0
+    for item in x:
+        if item == '':            
+            continue        
+        media += int(item)
+    media = media/5
+    print(media)
     
     f.close()
     
-    
-    
-
